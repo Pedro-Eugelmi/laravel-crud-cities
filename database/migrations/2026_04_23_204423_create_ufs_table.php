b60ae5524dfb6ee605b39a9083d34c1c2bc9e1a7
Pedro Eugelmi <pedrogabrielmendy@gmail.com>
2026-04-23 18:05:48 -0300

    feat: cria migration da tabela uf

---
+++ database/migrations/2026_04_23_204423_create_ufs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ufs', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->char('state_code', 2)->unique();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ufs');
+    }
+};
