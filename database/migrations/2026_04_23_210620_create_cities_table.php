7ff2ab4c186b962e103334efbd11e00c35fae299
Pedro Eugelmi <pedrogabrielmendy@gmail.com>
2026-04-23 18:14:16 -0300

    feat: cria migration da tabela cities

---
+++ database/migrations/2026_04_23_210620_create_cities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('zip_code', 8);
+            $table->char('ddd', 2);
+            $table->string('ibge_code', 7);
+            
+            $table->foreignId('uf_id')->constrained('ufs')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cities');
+    }
+};
